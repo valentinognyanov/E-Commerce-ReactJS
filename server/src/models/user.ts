da07d4cb3fed094dea8bd0cc7d418ca8beca8d7a
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-03 21:52:47 +0200

    create user chema

---
+++ server/src/models/user.ts
@@ -0,0 +1,18 @@
+import {Schema, model} from "mongoose";
+
+export interface IUser {
+    _id?: string;
+    username: string;
+    password: string;
+    avaliableMoney: number;
+    // purchasedItems: string[];
+}
+
+const UserSchema = new Schema<IUser>({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    avaliableMoney: {type: Number, default: 5000},
+    // purchasedItems:
+});
+
+export const UserModel = model<IUser>("user", UserSchema);
