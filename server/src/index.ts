61ab2e29df5eee4e87accb1ec30444da775589aa
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-07 21:31:38 +0200

    import router for auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import { userRouter } from "./routes/user";
+
 const PORT = 3001;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/user", userRouter);
 
-mongoose.connect("mongodb+srv://admin:ecommercepass@cluster0.apjhgah.mongodb.net/cluster0")
+mongoose.connect(
+    "mongodb+srv://admin:ecommercepass@cluster0.apjhgah.mongodb.net/cluster0"
+);
 
 app.listen(PORT, () => console.log(`Server is litening to port: ${PORT}`));
