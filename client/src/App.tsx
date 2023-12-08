5f47b385011c8155046fb8a4f947a1105d1ede1c
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-08 14:22:18 +0200

    create routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { Navbar } from "./components/navbar";
+
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/"></Route>
+                    <Route path="/auth"></Route>
+                    <Route path="/checkout"></Route>
+                    <Route path="/perchased-items"></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
