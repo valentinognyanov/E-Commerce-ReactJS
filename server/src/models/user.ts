b1547dd6f96a350b2893f659dcf431149b436213
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-09 01:02:45 +0200

    adding list of purchased items in user interface

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,18 +1,20 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
 export interface IUser {
     _id?: string;
     username: string;
     password: string;
     avaliableMoney: number;
-    // purchasedItems: string[];
+    purchasedItems: string[];
 }
 
 const UserSchema = new Schema<IUser>({
-    username: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    avaliableMoney: {type: Number, default: 5000},
-    // purchasedItems:
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avaliableMoney: { type: Number, default: 5000 },
+    purchasedItems: [
+        { type: Schema.Types.ObjectId, ref: "product", default: [] },
+    ],
 });
 
 export const UserModel = model<IUser>("user", UserSchema);
