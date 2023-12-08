62062cc7d5ad565f027092cf11816f3023285978
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-08 15:50:42 +0200

    create product schema

---
+++ server/src/models/product.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+export interface IProduct {
+    productName: string;
+    price: number;
+    description: string;
+    imageURL: string;
+    stockQuantity: number;
+}
+
+const ProductSchema = new Schema<IProduct>({
+    productName: { type: String, required: true },
+    price: {
+        type: Number,
+        required: true,
+        min: [1, "Price of product should be above 1."],
+    },
+    description: { type: String, required: true },
+    imageURL: { type: String, required: true },
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: [0, "Stock ca not be lower 0."],
+    },
+});
+
+export const ProductModel = model<IProduct>("product", ProductSchema);
