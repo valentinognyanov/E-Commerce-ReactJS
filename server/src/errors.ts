2a9837a76c047ee414d1aa3964949180ea42b992
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-09 01:03:08 +0200

    create enum for product error messages

--- server/src/errors.ts
+++ server/src/errors.ts
@@ -3,3 +3,9 @@ export enum UserErrors {
     WRONG_CREDENTIALS = "wrong-credentials",
     USERNAME_EXISTS = "username-exists",
 }
+
+export enum ProductErrors {
+    NO_PRODUCT_FOUND = "no-product-found",
+    NOT_ENOUGH_STOCK = "not-enough-stock",
+    NOT_ENOUGH_MONEY = "not-enough-money",
+}
