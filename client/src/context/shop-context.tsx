8bf4d6b63dc2cb99ed68575c9ef5bed32724b99d
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-11 21:00:40 +0200

    create shop context and provider

---
+++ client/src/context/shop-context.tsx
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react";
+
+export interface IShopContext {
+    addToCart: (itemId: string) => void;
+    removeFromCart: (itemId: string) => void;
+    updateCartItemCount: (newAmount: number, itemId: string) => void;
+    getCartItemCount: (itemId: string) => number;
+}
+
+const defaultValue: IShopContext = {
+    addToCart: () => null,
+    removeFromCart: () => null,
+    updateCartItemCount: () => null,
+    getCartItemCount: () => 0,
+};
+
+export const ShopContext = createContext<IShopContext>(defaultValue);
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState<{ string: number } | {}>({});
+
+    const getCartItemCount = (itemId: string): number => {
+        if (itemId in cartItems) {
+            return cartItems[itemId];
+        }
+
+        return 0;
+    };
+
+    const addToCart = (itemId: string) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+        } else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+        }
+    };
+    const removeFromCart = (itemId: string) => {};
+    const updateCartItemCount = (newAmount: number, itemId: string) => {};
+
+    const contextValue: IShopContext = {
+        addToCart,
+        removeFromCart,
+        updateCartItemCount,
+        getCartItemCount,
+    };
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    );
+};
