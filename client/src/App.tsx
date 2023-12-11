9523c3073dbb4e16981098c84ace2cb32a0767c0
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-11 21:00:58 +0200

    adding the provider to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,22 +6,28 @@ import { CheckoutView } from "./views/checkout";
 import { PerchasedItemsView } from "./views/perchased-items";
 import { ShopView } from "./views/shop";
 
+import { ShopContextProvider } from "./context/shop-context";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<ShopView />}></Route>
-                    <Route path="/auth" element={<AuthView />}></Route>
-                    <Route path="/checkout" element={<CheckoutView />}></Route>
-                    <Route
-                        path="/perchased-items"
-                        element={<PerchasedItemsView />}
-                    ></Route>
-                </Routes>
+                <ShopContextProvider>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<ShopView />}></Route>
+                        <Route path="/auth" element={<AuthView />}></Route>
+                        <Route
+                            path="/checkout"
+                            element={<CheckoutView />}
+                        ></Route>
+                        <Route
+                            path="/perchased-items"
+                            element={<PerchasedItemsView />}
+                        ></Route>
+                    </Routes>
+                </ShopContextProvider>
             </Router>
         </div>
     );
