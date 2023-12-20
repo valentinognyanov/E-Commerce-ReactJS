7dfaf048d1f74c6e45cb6a7a263bfdb3c39acaac
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-20 17:33:02 +0200

    create button for checkout and add funct to it

--- client/src/context/shop-context.tsx
+++ client/src/context/shop-context.tsx
@@ -1,10 +1,18 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+import { useGetProducts } from "../hooks/useGetProducts";
+import { IProduct } from "../models/interfaces";
+import { useGetToken } from "../hooks/useGetToken";
 
 export interface IShopContext {
     addToCart: (itemId: string) => void;
     removeFromCart: (itemId: string) => void;
     updateCartItemCount: (newAmount: number, itemId: string) => void;
     getCartItemCount: (itemId: string) => number;
+    getTotalCartAmount: () => number;
+    checkout: () => void;
 }
 
 const defaultValue: IShopContext = {
@@ -12,18 +20,22 @@ const defaultValue: IShopContext = {
     removeFromCart: () => null,
     updateCartItemCount: () => null,
     getCartItemCount: () => 0,
+    getTotalCartAmount: () => 0,
+    checkout: () => null,
 };
 
 export const ShopContext = createContext<IShopContext>(defaultValue);
 
 export const ShopContextProvider = (props) => {
     const [cartItems, setCartItems] = useState<{ string: number } | {}>({});
+    const { products } = useGetProducts();
+    const { headers } = useGetToken();
+    const navigate = useNavigate();
 
     const getCartItemCount = (itemId: string): number => {
         if (itemId in cartItems) {
             return cartItems[itemId];
         }
-
         return 0;
     };
 
@@ -46,11 +58,39 @@ export const ShopContextProvider = (props) => {
         setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
     };
 
+    const getTotalCartAmount = (): number => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo: IProduct = products.find(
+                    (product) => product._id === item
+                );
+                totalAmount += cartItems[item] * itemInfo.price;
+            }
+        }
+        return totalAmount;
+    };
+
+    const checkout = async () => {
+        const body = { customerId: localStorage.getItem("userID"), cartItems };
+
+        try {
+            await axios.post("http://localhost:3001/product/checkout", body, {
+                headers,
+            });
+            navigate("/");
+        } catch (error) {
+            console.log({error});
+        }
+    };
+
     const contextValue: IShopContext = {
         addToCart,
         removeFromCart,
         updateCartItemCount,
         getCartItemCount,
+        getTotalCartAmount,
+        checkout,
     };
     return (
         <ShopContext.Provider value={contextValue}>
--- client/src/views/checkout/index.tsx
+++ client/src/views/checkout/index.tsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router";
 
 import { CartItem } from "./cart-item";
 
-import { useGetProducts } from "../../hooks/useGetProducts";
 import { IProduct } from "../../models/interfaces";
 import { IShopContext, ShopContext } from "../../context/shop-context";
+
+import { useGetProducts } from "../../hooks/useGetProducts";
 
 import "./styles.css";
 
 export const CheckoutView = () => {
-    const { getCartItemCount } = useContext<IShopContext>(ShopContext);
+    const { getCartItemCount, getTotalCartAmount, checkout } =
+        useContext<IShopContext>(ShopContext);
     const { products } = useGetProducts();
+    const navigate = useNavigate();
+
+    const totalAmount = getTotalCartAmount();
 
     return (
         <div className="cart">
@@ -20,10 +26,23 @@ export const CheckoutView = () => {
             <div className="cart-body">
                 {products.map((product: IProduct) => {
                     if (getCartItemCount(product._id) !== 0) {
-                        return <CartItem product={product} />;
+                        return <CartItem key={product._id} product={product} />;
                     }
                 })}
             </div>
+            {totalAmount > 0 ? (
+                <div className="checkout">
+                    <p>Total price: ${totalAmount.toFixed(2)}</p>
+                    <button onClick={() => navigate("/")}>
+                        Continue Shopping
+                    </button>
+                    <button onClick={checkout}>Checkout</button>
+                </div>
+            ) : (
+                <h2 className="empty-shopping-cart">
+                    Your shopping cart is empty.
+                </h2>
+            )}
         </div>
     );
 };
