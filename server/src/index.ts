c2e71fc6530816f587bd6b41d34734f56a446bbf
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-21 18:37:57 +0200

    error handling when connecting to mongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,17 +6,31 @@ import { userRouter } from "./routes/user";
 import { productRouter } from "./routes/product";
 
 const PORT = 3001;
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/user", userRouter);
-app.use("/product", productRouter);
+app.use("/products", productRouter);
 
-mongoose.connect(
-    "mongodb+srv://admin:ecommercepass@cluster0.apjhgah.mongodb.net/cluster0"
-);
-
-app.listen(PORT, () => console.log(`Server is litening to port: ${PORT}`));
+mongoose
+    .connect(
+        "mongodb+srv://admin:ecommercepass@cluster0.apjhgah.mongodb.net/cluster0"
+    )
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () =>
+            console.log(`Server is listening on port: ${PORT}`)
+        );
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
